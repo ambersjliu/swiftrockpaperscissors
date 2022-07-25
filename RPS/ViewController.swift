a9e994d1c7910077fc4c047c7bad6cb0a2f4619d
amber liu <amber.sj.liu@gmail.com>
2022-07-24 21:45:56 -0400

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Swift Admin on 2022-07-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
